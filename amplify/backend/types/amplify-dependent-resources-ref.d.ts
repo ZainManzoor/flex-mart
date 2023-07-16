858bb42b6e7b236dee7b5fac0d8bb174f7cbd1e1
ZainManzoor <zain.manzoor@tkxel.com>
2023-07-17 02:28:29 +0500

    Added amplify api (added example schema for amplify api).

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "flexmart": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
